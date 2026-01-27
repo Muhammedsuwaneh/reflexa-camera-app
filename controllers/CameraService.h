12ca7b49bff0af66ed37f765007383c8fb4f8b50
Muhammedsuwaneh <suwanehmuhammed1@gmail.com>
2026-01-27 03:57:33 +0300

    video capture init

--- controllers/CameraService.h
+++ controllers/CameraService.h
@@ -159,7 +159,7 @@ private:
     cv::VideoCapture cap;
 
     FaceDetector faceDetector;
-    bool detectingFace = false;
+    std::atomic<bool> detectingFace = false;
 
     QTimer timer;
 
@@ -204,7 +204,7 @@ private:
 
     QString m_activeFilter = "";
     QImage m_recentCaptured;
-    bool m_capturingVideo = false;
+    std::atomic<bool> m_capturingVideo = false;
 
     cv::VideoWriter writer;
 };
